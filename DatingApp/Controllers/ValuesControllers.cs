21a94358605e010e51207615caa5e6f7e16e0adc
Lalji Goti <laljigoti@outlook.com>
2020-04-26 12:11:39 +0200

    section 4 complete

--- DatingApp/Controllers/ValuesControllers.cs
+++ DatingApp/Controllers/ValuesControllers.cs
@@ -23,6 +23,7 @@ namespace DatingApp.Controllers
         }
 
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
